c9b5e66f09970d529ed9e089aa799602884c7036
Tim Ermilov <yamalight@gmail.com>
2016-01-21 15:36:43 +0100

    only install new node modules if folder is not present closes #4

--- src/code/installNodeModule.js
+++ src/code/installNodeModule.js
@@ -1,4 +1,9 @@
+import {stat} from 'fs';
+import {join} from 'path';
 import {exec} from 'child_process';
 const execPromise = Promise.promisify(exec);
+const statPromise = Promise.promisify(stat);
 
-export const installNodeModule = (name) => execPromise(`npm install ${name}`);
+export const installNodeModule = (name) =>
+    statPromise(join(__dirname, '..', '..', 'node_modules', name))
+    .catch(() => execPromise(`npm install ${name}`));
